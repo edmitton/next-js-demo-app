aaf48fb177fad6ff39ffa71ad1dd0ff60ee2eaf2
MitomiTaisei <mton.marvel@gmail.com>
2025-01-06 23:31:52 +0900

    create tutorial tic tac toe

---
+++ app/tutorial/page.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import './style.css';
+import { useState } from "react";
+
+function Square({ value, onSquareClick }: {
+  value: string | null;
+  onSquareClick: () => void;
+}) {
+  // const [value, setValue] = useState<string | null>(null);
+
+  // function handleClick() {
+  //   const nextSquares = squares.slice();
+  //   nextSquares[0] = 'X';
+  // }
+
+  return(
+    <button
+      className='square'
+      onClick={onSquareClick}
+    >
+      { value }
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i: number ) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Hello Tutorial!</h1>
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
